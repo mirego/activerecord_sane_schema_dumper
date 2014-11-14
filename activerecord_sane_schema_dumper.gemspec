5375b6f926f9fd85b36c8d214f2cf016aa738cdc
Rémi Prévost <remi@exomel.com>
2014-11-14 13:03:49 -0500

    Add Phare and Rubocop

--- activerecord_sane_schema_dumper.gemspec
+++ activerecord_sane_schema_dumper.gemspec
@@ -20,6 +20,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.7'
   spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rubocop', '0.27.1'
+  spec.add_development_dependency 'phare'
 
   spec.add_dependency 'rails', '>= 4', '< 5'
 end
--- lib/active_record/sane_schema_dumper/railtie.rb
+++ lib/active_record/sane_schema_dumper/railtie.rb
@@ -1,11 +1,13 @@
 require 'activerecord_sane_schema_dumper'
 require 'rails'
 
-module ActiveRecord::SaneSchemaDumper
-  class Railtie < Rails::Railtie
-    initializer 'activerecord_sane_schema_dumper.active_record' do
-      ActiveSupport.on_load :active_record do
-        ActiveRecord::SchemaDumper.send(:include, ActiveRecord::SaneSchemaDumper::Extension)
+module ActiveRecord
+  module SaneSchemaDumper
+    class Railtie < Rails::Railtie
+      initializer 'activerecord_sane_schema_dumper.active_record' do
+        ActiveSupport.on_load :active_record do
+          ActiveRecord::SchemaDumper.send(:include, ActiveRecord::SaneSchemaDumper::Extension)
+        end
       end
     end
   end
